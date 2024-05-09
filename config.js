7c09f7bb8e2697ebf7471cf1fe266b423292c219
bubblerat <148023829+bubblerat@users.noreply.github.com>
2024-05-09 11:23:58 +1000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const firebase = require("firebase");
+const firebaseConfig = {
+  apiKey: "AIzaSyCINZ0efQUYv_588y_uwaxOgMpZFsFOO5Q",
+  authDomain: "food-app-fa0fd.firebaseapp.com",
+  projectId: "food-app-fa0fd",
+  storageBucket: "food-app-fa0fd.appspot.com",
+  messagingSenderId: "1049057631117",
+  appId: "1:1049057631117:web:1d881dd376aed5eee23bc4"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const User = db.collection("users");
+const Category = db.collection("categories");
+const Dishes = db.collection("dishes");
+const Orders = db.collection("orders");
+module.exports = { User, Category, Dishes, Orders };
+dfgfdgsdfgsfdg
+
+
+
